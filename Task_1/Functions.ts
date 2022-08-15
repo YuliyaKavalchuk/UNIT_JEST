9fccc58f3411dcfd3a3a1214655c6c118d696b85
YuliyaKavalchuk <yuliya.kavalchuk@gmail.com>
2022-08-15 12:47:19 +0300

    6th commit refactoring

--- Task_1/Functions.ts
+++ Task_1/Functions.ts
@@ -1,4 +1,5 @@
 import { alphabet, specialChar, emailTemplate } from "./const/functionsConst";
+import { emailSeparator, spaceInput } from "./const/expectedResultConst";
 
 export function generateRandomLetterLowerCase(): string {
     return alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -12,8 +13,8 @@ export function getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function getrandomString(length: number): string {
-    let result: string = "";
+export function getRandomString(length: number): string {
+    let result: string = spaceInput;
     let characters: string = alphabet + alphabet.toUpperCase();
     let charactersLength: number = characters.length;
     for (let i = 0; i < length; i++) {
@@ -23,8 +24,8 @@ export function getrandomString(length: number): string {
 }
 
 export function getEmail(): string {
-    let emailSplit: any = emailTemplate.split("@");
-    return emailSplit[0] + getRandomInt(10000, 99999) + "@" + emailSplit[1];
+    let emailSplit: any = emailTemplate.split(emailSeparator);
+    return emailSplit[0] + getRandomInt(10000, 99999) + emailSeparator + emailSplit[1];
 }
 export function getPassword(): string {
     let min: number = 0;
@@ -34,5 +35,5 @@ export function getPassword(): string {
 }
 
 export function getFullName(): string {
-    return getrandomString(getRandomInt(1, 14)) + " " + getrandomString(getRandomInt(1, 15));
+    return getRandomString(getRandomInt(1, 14)) + spaceInput + getRandomString(getRandomInt(1, 15));
 }
